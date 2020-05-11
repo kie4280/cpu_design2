ba852bf5b81d2f521c9da2922fb69fb5b47904d9
HoChuanYu <hoalex8599@gmail.com>
2020-05-11 22:04:52 +0800

    finish Sign_Extend

--- sample_code/Sign_Extend.v
+++ sample_code/Sign_Extend.v
@@ -2,15 +2,22 @@
 
 module Sign_Extend(
     data_i,
+    sign,
     data_o
     );
 
 //I/O ports
 input   [16-1:0] data_i;
+input   sign;
 output  [32-1:0] data_o;
 
 //Internal Signals
 reg     [32-1:0] data_o;
+if (sign==1) begin
+    data_o={{data_i[15],16},data_i};
+end else begin
+    data_o={{0,16},data_i};
+end
 
 //Sign extended
 
