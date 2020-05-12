0e53396860d86234c26e7305484197f90deca6c2
HoChuanYu <hoalex8599@gmail.com>
2020-05-12 21:35:55 +0800

    programcount

--- sample_code/Simple_Single_CPU.v
+++ sample_code/Simple_Single_CPU.v
@@ -10,34 +10,36 @@ input clk_i;
 input rst_i;
 
 wire [32-1:0] instruction;
-wire [32-1:0] ProgramCounter;
+wire [32-1:0] ProgramCounter_i, ProgramCounter_o, ProgramCounter_4
+              ProgramCounter_b, ProgramCounter_w, ProgramCounter_4w;
 wire [5-1:0] reg_write_addr;
-wire reg_write, reg_dst, alu_src, branch;
+wire reg_write, reg_dst, alu_src, branch, zero;
 wire [3-1:0] alu_ctrl;
+
 
 ProgramCounter PC(
     .clk_i(clk_i),
     .rst_i (rst_i),
-    .pc_in_i(),
-    .pc_out_o(ProgramCounter)
+    .pc_in_i(ProgramCounter_i),//
+    .pc_out_o(ProgramCounter_o)
     );
 
 Adder Adder1(
-    .src1_i(),
-    .src2_i(),
-    .sum_o()
+    .src1_i(ProgramCounter_o),//
+    .src2_i(32'd4),//
+    .sum_o(ProgramCounter_4)//
     );
 
 Instr_Memory IM(
-    .pc_addr_i(ProgramCounter),
+    .pc_addr_i(ProgramCounter_o),
     .instr_o(instruction)
     );
 
 MUX_2to1 #(.size(5)) Mux_Write_Reg(
     .data0_i(instruction[20:16]),
     .data1_i(instruction[15:11]),
-    .select_i(),
-    .data_o()
+    .select_i(reg_dst),//
+    .data_o(instruction[15:11])  //
     );
 
 Reg_File RF(
@@ -46,10 +48,10 @@ Reg_File RF(
     .RSaddr_i(instruction[25:21]) ,
     .RTaddr_i(instruction[20:16]) ,
     .RDaddr_i(instruction[15:11]) ,
-    .RDdata_i()  ,
-    .RegWrite_i (),
-    .RSdata_o() ,
-    .RTdata_o()
+    .RDdata_i(0) ,//
+    .RegWrite_i (reg_write),//
+    .RSdata_o() ,//
+    .RTdata_o()//
     );
 
 Decoder Decoder(
@@ -85,13 +87,13 @@ ALU ALU(
     .src2_i(),
     .ctrl_i(),
     .result_o(),
-    .zero_o()
+    .zero_o(zero)
     );
 
 Adder Adder2(
-    .src1_i(),
+    .src1_i(ProgramCounter_w),
     .src2_i(),
-    .sum_o()
+    .sum_o(ProgramCounter_4w)
     );
 
 Shift_Left_Two_32 Shifter(
@@ -100,10 +102,10 @@ Shift_Left_Two_32 Shifter(
     );
 
 MUX_2to1 #(.size(32)) Mux_PC_Source(
-    .data0_i(),
+    .data0_i(ProgramCounter_4w),
     .data1_i(),
-    .select_i(),
-    .data_o()
+    .select_i(zero&branch),
+    .data_o(ProgramCounter_i)
     );
 
 endmodule
